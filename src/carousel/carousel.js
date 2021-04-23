52742902552cdac3de58076d721d6173d6b5cd9a
Lursmani <lursmani9@gmail.com>
2021-04-23 15:10:53 +0400

    updated v1

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -1,80 +1,100 @@
-import React, {useState, useEffect} from "react"
-import "./carousel.css"
-
-
-
+import React, { useState, useEffect } from "react";
+import "./carousel.css";
 
 function Carousel(props) {
-    const {children} = props
-    const [index, setIndex] = useState(0)
-    const [length, setLength] = useState(children.length)
-    const mobile = window.innerWidth > 768 ? "false" : "true"
-    function breakpoints() {
-        let x
-        if (window.innerWidth <= 480) { return x === "mobile" } else 
-        if (window.innerWidth > 480 && window.innerWidth <= 768) {return x === "tablet"} else 
-        if (window.innerWidth > 768) {return x === "desktop"}
-        return x
+  const { children } = props;
+  const [index, setIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+  const [clickStart, setClickStart] = useState();
+  const [dragging, setDragging] = useState(false);
+  const mobile = window.innerWidth > 768 ? "false" : "true";
+
+  useEffect(() => {
+    setLength(children.length);
+  }, [children]);
+
+  const next = () => {
+    if (index < length - 1) {
+      setIndex((prevState) => prevState + 1);
     }
-    const breakpoint = breakpoints()
-    const itemWidth = () => {
-        let x = props.show
-        let width = 100 / x
-        return width
+    // sets the index to next if you are not on the last slide
+  };
+
+  const previous = () => {
+    if (index > 0) {
+      setIndex((prevState) => prevState - 1);
     }
+    // sets the index to be the previous if you are further than first slide
+  };
 
+  const handleMouseDown = (e) => {
+    setClickStart(e.screenX);
+    console.log(e.screenX);
+  };
 
-    useEffect(() => {
-        setLength(children.length)
-    }, [children])
-
-
-
-
-    const next = () => {
-        if (index < (length-1))  {
-            setIndex(prevState => prevState + 1)
-        }
-        // sets the index to next if you are not on the last slide
+  const handleMouseMove = (e) => {
+    if (clickStart !== undefined) {
+      setDragging(true);
     }
+  };
 
-    const previous = () => {
-        if (index > 0) {
-            setIndex(prevState => prevState - 1)
-        }
-        // sets the index to be the previous if you are further than first slide
+  const handleMouseUp = (e) => {
+    if (clickStart - e.screenX > 20 && index < children.length - props.show) {
+      next();
+    } else if (clickStart - e.screenX < -20) {
+      previous();
     }
+    setClickStart();
+    setDragging(false);
+    console.log(e.screenX);
+  };
 
-  
-
-    return (
-        <div className="carousel-outer-container" style={{
-            height: props.outerHeight, 
-            width: props.outerWidth,
-            padding: props.outerPadding,
-            }}>
-        <div className="carousel-inner-container" style={{height: props.innerHeight, width: props.innerWidth}}>
-            <div className="carousel-wrapper"        
-            > 
-               { index > 0 && <button className="left-arrow" onClick={previous}>
-                &lt;
-                </button> }
-                <div className="carousel-content-wrapper">
-               { index < children.length - props.show &&    <button className="right-arrow" onClick={next}>
-                    &gt;
-                </button>}
-                {/* the sliding is done by the translateX below. it translates by (100% of the slides * the index of the slide) from the starting position.  */}
-                    <div className="carousel-content"
-                    style={{transform: mobile === "false" && `translateX(-${index * 100 }%)`,
-                            width: mobile === "false" && `${100 / props.show}%`}}
-                    >
-                        { children}
-                    </div>
-                </div>
+  return (
+    <div
+      className="carousel-outer-container"
+      style={{
+        height: props.outerHeight,
+        width: props.outerWidth,
+        padding: props.outerPadding,
+      }}
+    >
+      <div
+        className="carousel-inner-container"
+        style={{ height: props.innerHeight, width: props.innerWidth }}
+      >
+        <div className="carousel-wrapper">
+          {index > 0 && (
+            <button className="left-arrow" onClick={previous}>
+              &lt;
+            </button>
+          )}
+          {index < children.length - props.show && (
+            <button className="right-arrow" onClick={next}>
+              &gt;
+            </button>
+          )}
+          <div
+            className="carousel-content-wrapper"
+            onMouseMove={(e) => handleMouseMove(e)}
+            onMouseDown={(e) => handleMouseDown(e)}
+            onMouseUp={(e) => handleMouseUp(e)}
+          >
+            {/* the sliding is done by the translateX below. it translates by (100% of the slides width * the index of the slide) from the starting position.  */}
+            <div
+              className="carousel-content"
+              style={{
+                transform: mobile === "false" && `translateX(-${index * 100}%)`,
+                width: mobile === "false" && `${100 / props.show}%`,
+                pointerEvents: dragging ? "none" : undefined,
+              }}
+            >
+              {children}
             </div>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/carousel/demo/carouselView.js
+++ src/carousel/demo/carouselView.js
@@ -1,84 +1,94 @@
-import React, {useState} from "react"
-import "../carousel.css"
-import "./view.css"
-import CarouselWrapper from "../carousel"
-import Slide from "../slide"
-
-
+import React, { useState } from "react";
+import "../carousel.css";
+import "./view.css";
+import CarouselWrapper from "../carousel";
 
 function Slide1() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    return (
-        <div id="cardem-slide1">
-            <div id="slide1-content">
-            <h1>This is my React Carousel</h1>
-            <h3>This carousel can accept any content.</h3>
-            <h3>For example, this simple counter component:</h3>
-            <div id="cardem-counterdiv">
-                <p  id="cardem-counternum">Count = {count}</p>
-                <button className="cardem-button" id="cardem-buttonP" onClick={() => setCount(count + 1)}>Add 1</button>
-                <button className="cardem-button" id="cardem-buttonM" onClick={() => setCount(count - 1)}>Subtract 1</button>
-                <button  className="cardem-button" id="cardem-buttonR" onClick={()=> setCount(0)}>Reset</button>
-            </div>
-            </div>
+  return (
+    <div id="cardem-slide1">
+      <div id="slide1-content">
+        <h1 className="cardem-slide1-text">This is my React Carousel</h1>
+        <h3 className="cardem-slide1-text">
+          This carousel can accept any content.
+        </h3>
+        <h3 className="cardem-slide1-text">
+          For example, this simple counter component:
+        </h3>
+        <div id="cardem-counterdiv">
+          <p id="cardem-counternum">Count = {count}</p>
+          <button
+            className="cardem-button"
+            id="cardem-buttonP"
+            onClick={() => setCount(count + 1)}
+          >
+            Add 1
+          </button>
+          <button
+            className="cardem-button"
+            id="cardem-buttonM"
+            onClick={() => setCount(count - 1)}
+          >
+            Subtract 1
+          </button>
+          <button
+            className="cardem-button"
+            id="cardem-buttonR"
+            onClick={() => setCount(0)}
+          >
+            Reset
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
-
 
 function CarouselView() {
-
-
-    return (
-       
-      <CarouselWrapper 
+  return (
+    <CarouselWrapper
       outerHeight="100%"
       outerWidth="100%"
-      innerHeight="100%" 
+      innerHeight="100%"
       show={2}
-      >
-          <Slide slideInnerWidth="98%" >
-              <Slide1 />
-          </Slide>
-     
-          <Slide slideInnerWidth="98%">
-              <div id="cardem-slide2">
-                <div id="cardem-slide2-textdiv">
-               <p id="cardem-slide2-text">It can also accept images.</p>
-               </div>
-              </div>
-          </Slide>
-          <Slide slideInnerWidth="98%">
-              <div id="cardem-slide3">
-                  <p className="cardem-slide3-text">
-                      Or anything else you can imagine. 
-                  </p>
-                  <p className="cardem-slide3-text">
-                        It is customizable, responsive, easy to use, and fast. 
-                  </p>
-                  <p className="cardem-slide3-text">
-                      So, if you like it and want to use it yourself, you can find it on my github <a id="cardem-slide3-link" href="https://github.com/Lursmani/simple-carousel">repo</a>. Instructions can be found in the repo, and also in the "how to use" file within the app. 
-                  </p>
-                  <p className="cardem-slide3-text">
-                        Now you can go to the next slide and watch a very cute video. 
-                  </p>
-              </div>
-          </Slide>
-               
-          <Slide slideInnerWidth="98%">
-              <div id="cardem-slide4">
-              <iframe id="cardem-video" height="100%" src="https://www.youtube.com/embed/7yLxxyzGiko" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </div>
-          </Slide>
+    >
+      <Slide1 />
 
-      </CarouselWrapper>
-      
-    )
+      <div id="cardem-slide2">
+        <div id="cardem-slide2-textdiv">
+          <p id="cardem-slide2-text">It can also accept images.</p>
+        </div>
+      </div>
 
+      <div id="cardem-slide3">
+        <p className="cardem-slide3-text">Or anything else you can imagine.</p>
+        <p className="cardem-slide3-text">
+          It is customizable, responsive, easy to use, and fast.
+        </p>
+        <p className="cardem-slide3-text">
+          So, if you like it and want to use it yourself, you can find it on my
+          github{" "}
+          <a id="cardem-slide3-link" href="https://github.com/Lursmani/simple-carousel">repo</a>.
+        </p>
+        <p className="cardem-slide3-text">
+          Or, go to the next slide and watch a very cute video.
+        </p>
+      </div>
 
+      <div id="cardem-slide4">
+        <iframe
+          id="cardem-video"
+          height="100%"
+          src="https://www.youtube.com/embed/7yLxxyzGiko"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </CarouselWrapper>
+  );
 }
 
-
-export default CarouselView
+export default CarouselView;
